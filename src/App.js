c1af4187d50e6e2586769289715447c179d5d1d2
w3docs <w3docs@>
2022-11-08 12:08:53 -0800

    test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, dad!
         </p>
         <a
           className="App-link"
